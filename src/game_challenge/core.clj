12d19be4d292337f1b34d89bd3332eb1031196c8
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-09-06 17:29:45 +0200

    Added maven build for cloned project.

--- src/game_challenge/core.clj
+++ src/game_challenge/core.clj
@@ -4,22 +4,27 @@
             [clojure.java.shell :as shell]
             [game-challenge.util :as util]))
 
-(defn find-git-in-path
+(defn find-in-path
   "Searches for git executable in $PATH"
-  []
+  [executable]
   (let [paths (string/split (System/getenv "PATH") #":")
-        candidates (map #(io/file % "git") paths)]
+        candidates (map #(io/file % executable) paths)]
     (first (filter #(.exists %) candidates))))
 
-(def ^:dynamic *git* (.getAbsolutePath (find-git-in-path)))
+(def ^:dynamic *git* (.getAbsolutePath (find-in-path "git")))
+(def ^:dynamic *mvn* (.getAbsolutePath (find-in-path "mvn")))
 
 (def ^:dynamic *build-dir* (util/mk-tmp-dir!))
 
-(defn git
+(defn- git
   [command & args]
   (apply shell/sh (cons *git* (cons command args))))
 
-(defn- project-build-dir
+(defn- maven
+  [command & args]
+  (apply shell/sh (cons *mvn* (cons command args))))
+
+(defn project-build-dir
   [project]
   (let [name (:name project)]
     (.getAbsolutePath (io/file *build-dir* name))))
@@ -30,7 +35,8 @@
         target (project-build-dir project)]
     (git "clone" repo target)))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn maven-build!
+  [project]
+  (let [build-dir (project-build-dir project)]
+    (shell/with-sh-dir build-dir
+      (maven "package"))))
--- test/game_challenge/core_test.clj
+++ test/game_challenge/core_test.clj
@@ -6,9 +6,15 @@
 
 (deftest test-find-git-in-path
   (testing "that path is searched for git"
-    (is (= (io/file "/usr/bin/git") (find-git-in-path)))))
+    (is (= (io/file "/usr/bin/git") (find-in-path "git")))))
 
 
 (deftest test-clone-project-repo
   (testing "that a repository is successfully cloned"
     (is (= 0 (:exit (clone-project! {:repository "https://github.com/TheCodEx/game-challenge.git" :name "game-challenge"}))))))
+
+(deftest test-maven-build!
+  (testing "that a maven build gets executed"
+    (let [project {:repository "https://github.com/TheCodEx/logparser.git" :name "logparser"}
+          repo (clone-project! project)]
+      (is (= 0 (:exit (maven-build! project)))))))
