18cf82de3097ac307bd2c0cc917f32659f98ce92
Folker Bernitt <folker-bernitt-github@gmx.de>
2013-09-06 15:58:38 +0200

    Added *git* global.

--- src/game_challenge/core.clj
+++ src/game_challenge/core.clj
@@ -1,7 +1,18 @@
 (ns game-challenge.core
-  (:require [cemerick.pomegranate.aether :as aether]
+  (:require [clojure.string :as string]
+            [clojure.java.io :as io]
+            [cemerick.pomegranate.aether :as aether]
             [cemerick.pomegranate :as pomegranate]
             [pedantic.core :as pedantic]))
+
+(defn find-git-in-path
+  []
+  (let [paths (string/split (System/getenv "PATH") #":")
+        candidates (map #(io/file % "git") paths)]
+    (first (filter #(.exists %) candidates))))
+
+(def ^:dynamic *git* (find-git-in-path))
+
 
 (defn foo
   "I don't do a whole lot."
--- test/game_challenge/core_test.clj
+++ test/game_challenge/core_test.clj
@@ -1,4 +1,9 @@
 (ns game-challenge.core-test
   (:require [clojure.test :refer :all ]
-            [game-challenge.core :refer :all ]))
+            [game-challenge.core :refer :all ]
+            [clojure.java.io :as io]))
 
+
+(deftest test-find-git-in-path
+  (testing "that path is searched for git"
+    (is (= (io/file "/usr/bin/git") (find-git-in-path)))))
